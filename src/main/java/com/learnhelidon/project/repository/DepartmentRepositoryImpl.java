c4dd3aa973d772b90f5cab82b9cea8d1a14b315a
Krishna Lagad <krishnalagad2@gmail.com>
2023-11-09 14:40:30 +0530

    - dump implementation for findall()

--- src/main/java/com/learnhelidon/project/repository/DepartmentRepositoryImpl.java
+++ src/main/java/com/learnhelidon/project/repository/DepartmentRepositoryImpl.java
@@ -1,13 +1,39 @@
 package com.learnhelidon.project.repository;
 
+import com.learnhelidon.project.config.AppConstants;
 import com.learnhelidon.project.entity.Department;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DepartmentRepositoryImpl implements DepartmentRepository{
+
+    private Connection connection;
+
+    public DepartmentRepositoryImpl(Connection connection) {
+        this.connection = connection;
+    }
+
     @Override
     public List<Department> findAll() {
-        return null;
+        List<Department> departments = new ArrayList<>();
+        try{
+            Statement statement = this.connection.createStatement();
+            String query = "SELECT * FROM " + AppConstants.TABLE_NAME;
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                Department department = new Department(resultSet.getLong("id"),
+                        resultSet.getString("name"));
+                departments.add(department);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return departments;
     }
 
     @Override
