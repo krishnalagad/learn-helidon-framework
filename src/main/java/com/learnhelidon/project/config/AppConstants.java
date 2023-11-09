6dba7ec8a9efcea2ef16afc5e285f512f952aebe
Krishna Lagad <krishnalagad2@gmail.com>
2023-11-09 14:40:11 +0530

    - dump App constants

---
+++ src/main/java/com/learnhelidon/project/config/AppConstants.java
@@ -0,0 +1,11 @@
+package com.learnhelidon.project.config;
+
+public class AppConstants {
+    // mysql config
+    public static final String DB_URL = "";
+    public static final String DB_USERNAME = "";
+    public static final String DB_PASSWORD = "";
+
+    // database config
+    public static final String TABLE_NAME = "helidon_dept";
+}
