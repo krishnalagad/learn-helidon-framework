ce098d65f0881dab2ec45c2fc2a58a0d8f936c73
Krishna Lagad <krishnalagad2@gmail.com>
2023-11-09 14:41:41 +0530

    - dump actual values of url,username,password

--- src/main/java/com/learnhelidon/project/config/AppConstants.java
+++ src/main/java/com/learnhelidon/project/config/AppConstants.java
@@ -2,9 +2,9 @@ package com.learnhelidon.project.config;
 
 public class AppConstants {
     // mysql config
-    public static final String DB_URL = "";
-    public static final String DB_USERNAME = "";
-    public static final String DB_PASSWORD = "";
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/revise";
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "krishna24";
 
     // database config
     public static final String TABLE_NAME = "helidon_dept";
