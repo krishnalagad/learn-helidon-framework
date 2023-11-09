ae25fcd7e874ea714ae48dc0f831004d635c63c2
Krishna Lagad <krishnalagad2@gmail.com>
2023-11-09 14:24:58 +0530

    - dump entity class to hold data

---
+++ src/main/java/com/learnhelidon/project/entity/Department.java
@@ -0,0 +1,13 @@
+package com.learnhelidon.project.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Department {
+    private Long id;
+    private String name;
+}
