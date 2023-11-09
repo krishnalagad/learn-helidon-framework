9782b1d1c8d15c70452de3f012ec60c51332c4f9
Krishna Lagad <krishnalagad2@gmail.com>
2023-11-09 14:26:54 +0530

    - dump implementation class for repository

---
+++ src/main/java/com/learnhelidon/project/repository/DepartmentRepositoryImpl.java
@@ -0,0 +1,27 @@
+package com.learnhelidon.project.repository;
+
+import com.learnhelidon.project.entity.Department;
+
+import java.util.List;
+
+public class DepartmentRepositoryImpl implements DepartmentRepository{
+    @Override
+    public List<Department> findAll() {
+        return null;
+    }
+
+    @Override
+    public Department findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Department save(Department department) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
